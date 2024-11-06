3e9e7384ed0d9a3bfb1861fabb09696278abcf63
ARZSeries <andytrian6@gmail.com>
2024-11-06 16:09:41 +0700

    Penambahan Fitur Parallax
    Parallax di Lembar Pertama

--- script1.js
+++ script1.js
@@ -16,3 +16,22 @@ const navSlides = () => {
 };
 
 navSlides();
+
+//Parallax Effects
+let moon = document.getElementById("moon");
+let text = document.getElementById("text");
+let fish = document.getElementById("fish");
+
+let moonlight = document.getElementById("moonlight");
+let whale = document.getElementById("whale");
+
+
+window.addEventListener("scroll", () => {
+    let value = window.scrollY;
+    moon.style.top = value * .9 + "px";
+    text.style.top = 99 + value * -0.2 + '%';
+    fish.style.left = -1000 + value * 3.5 + "px";
+
+    moonlight.style.top = value * .3 + "px";
+    whale.style.left = value * .6 + "px";
+})
